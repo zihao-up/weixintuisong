765895246cbdb2398977f9c46230137969249152
zihao-up <102942913+zihao-up@users.noreply.github.com>
2022-11-14 18:36:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -409,8 +409,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
 
 def handler(event, context):
     try:
-        txt = "/home/ubuntu/pythonproject/myhao/config.txt"
-        with open(txt, encoding="utf-8") as f:
+        with open("config.txt", encoding="utf-8") as f:
             config = eval(f.read())
     except FileNotFoundError:
         print("推送消息失败，请检查config.txt文件是否与程序位于同一路径")
